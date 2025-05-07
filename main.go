7c87735db7a37c0b9873c6c39f95db023874d60c
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-05-07 20:20:33 +0200

    Added health handler on url /healthz and updated file server url to /app/

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import "net/http"
 
 func main() {
+	const (
+		filePathRoot = "."
+		port         = "8080"
+	)
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	serveMux.HandleFunc("/healthz", handleHealth)
 
 	httpServer := http.Server{
 		Handler: serveMux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
 
 	httpServer.ListenAndServe()
+}
+
+func handleHealth(responseWriter http.ResponseWriter, _ *http.Request) {
+	responseWriter.WriteHeader(200)
+	responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responseWriter.Write([]byte(http.StatusText(http.StatusOK)))
 }
