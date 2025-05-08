0a58a8cde088a2ba4e28b13ff22ee3a0ab6d3686
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-05-08 12:16:09 +0200

    Added metrics get/reset and middlware

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
 func main() {
 	const (
@@ -8,9 +12,15 @@ func main() {
 		port         = "8080"
 	)
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot)))))
 	serveMux.HandleFunc("/healthz", handleHealth)
+	serveMux.HandleFunc("/metrics", apiCfg.handleGetMetrics)
+	serveMux.HandleFunc("/reset", apiCfg.handleResetMetrics)
 
 	httpServer := http.Server{
 		Handler: serveMux,
@@ -24,4 +34,33 @@ func handleHealth(responseWriter http.ResponseWriter, _ *http.Request) {
 	responseWriter.WriteHeader(200)
 	responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	responseWriter.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	middlewareHandler := func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(middlewareHandler)
+}
+
+func (cfg *apiConfig) handleGetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+
+	message := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) handleResetMetrics(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Swap(0)
+
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("Successfuly reset request counter."))
 }
